9469c1bbe516981cfe11d6bf991cd8f586fad718
yurika-git <yurika_imanishi@icloud.com>
2020-12-29 17:18:28 +0900

    スペル訂正

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -7,7 +7,7 @@ function check() {
     post.setAttribute("data-load", "true");
     post.addEventListener("click", () => {
       const postId = post.getAttribute("data-id");
-      const XHR = new XHLHttpRequest();
+      const XHR = new XMLHttpRequest();
       XHR.open("GET", `/posts/${postId}`, true);
       XHR.responseType = "json";
       XHR.send();
